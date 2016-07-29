a9e9ce3c6d691729f72e7d26818c6224d72e8f26
Félix Leblanc <felix.leblanc1305@gmail.com>
2016-07-29 19:08:36 -0400

    Change hours

--- lights.py
+++ lights.py
@@ -7,19 +7,40 @@ import signal
 import sys
 
 relay = 23
+fan = 18
+
+start = "8:00"
+close = "23:00"
+
+start_hour = int(start.split(":")[0])
+close_hour = int(close.split(":")[0])
 
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(relay, GPIO.OUT)
+GPIO.setup(fan, GPIO.OUT)
+
 
 
 def lights_on():
     print("Lights on")
-    GPIO.output(relay, False)
+    GPIO.output(relay, True)
 
 
 def lights_off():
     print("Lights off")
     GPIO.output(relay, True)
+
+
+def fan_on():
+    now = datetime.datetime.now()
+    if now.hour > start_hour and now.hour < close_hour:
+        GPIO.output(fan, False)
+        time.sleep(300000)
+        fan_off()
+
+
+def fan_off():
+    GPIO.output(fan, True)
 
 
 def signal_handler(sig, frame):
@@ -28,20 +49,18 @@ def signal_handler(sig, frame):
     sys.exit(0)
 
 
-start = "8:00"
-close = "23:00"
-
 current_hour = datetime.datetime.now().hour
-start_hour = int(start.split(":")[0])
-close_hour = int(close.split(":")[0])
+
 
 if current_hour < start_hour or current_hour > close_hour:
     lights_off()
+    fan_off()
 else:
     lights_on()
 
 schedule.every().day.at("8:00").do(lights_on)
 schedule.every().day.at("23:00").do(lights_off)
+schedule.every().hour.do(fan_on)
 
 signal.signal(signal.SIGINT, signal_handler)
 
